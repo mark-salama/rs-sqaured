8807f1f19927dc2ba45bbd8a052e7564c6999318
mark-salama <mark.salama@columbia.edu>
2018-09-13 10:52:59 -0400

    reduced files sizes, removed results slider, changed fonts

--- app.py
+++ app.py
@@ -10,16 +10,26 @@ movie_factors = np.loadtxt("movie_factors.csv", delimiter=",")
 
 df = pd.read_csv('movie_dict.csv')
 
-def make_dash_table(df):
+def make_dash_table(df,number_input):
     ''' Return a dash definition of an HTML table for a Pandas dataframe '''
     html_row = [html.Th('Rank'),html.Th('Movie'),html.Th('Rating')]
     table = []
     table.append(html.Tr(html_row))
 
+    if number_input < 3:
+        html_row = [html.Td(),html.Th("""Input more movies to improve recommendations"""),html.Td()]
+        table.append(html.Tr(html_row))
+
+    # rows=[{'Rank': index+1,'Movie':row.movie_name,'Rating':round(row.avg_rating,2)} for index,row in df.iterrows()]
+    # table.append(rows)
+
     for index,row in df.iterrows():
         html_row = [html.Td(index+1),html.Td(row.movie_name),html.Td(round(row.avg_rating,2))]
         table.append(html.Tr(html_row))
+
     return table
+
+# app = dash.Dash()
 
 app = dash.Dash(__name__)
 server = app.server
@@ -38,11 +48,8 @@ app.index_string = '''
         {%app_entry%}
         <footer>
         
-        Movie features were learned using the KNN similarity function provided by <a href="http://surpriselib.com">Surprise</a>. Ratings data from the <a href="https://grouplens.org/datasets/movielens/">MovieLens dataset</a>, citation below. 
-
-        <br>
-
-        F. Maxwell Harper and Joseph A. Konstan. 2015. The MovieLens Datasets: History and Context. ACM Transactions on Interactive Intelligent Systems (TiiS) 5, 4, Article 19 (December 2015), 19 pages. DOI=http://dx.doi.org/10.1145/2827872
+        Similarities between movies were learned using the KNN algorithm provided by <a href="http://surpriselib.com">Surprise</a>
+        with ratings data from the <a href="https://grouplens.org/datasets/movielens/">MovieLens</a> dataset. 
 
             {%config%}
             {%scripts%}
@@ -56,12 +63,11 @@ app.layout = html.Div(children=[
 
     html.Div([  
 
-        html.H2("""Find Movies You'll Love""",
+        html.H1("""Find Movies You'll Love""",
             style={
                 'text-align': 'center' ,
                 'margin-left': 'auto',
                 'margin-right': 'auto' 
-
             }
         ),
 
@@ -69,11 +75,12 @@ app.layout = html.Div(children=[
 
             dcc.Dropdown(
                 id='movie-selector',
-                options=[{'label': rows['movie_name'], 'value': rows['matrix_row']} for index,rows in df.sort_values('avg_rating',ascending=False).iterrows()],
+                options=[{'label': rows['movie_name'], 'value': index} for index,rows in df.sort_values('avg_rating',ascending=False).iterrows()],
+                placeholder="Select at least one movie...",
                 multi=True),
     
         ],
-        style={'width': '75%', 'display': 'inline-block', 'marginBottom':15}
+        style={'width':'75%', 'margin-left':'15%', 'margin-right':'15%','text-align': 'left',}
         ),
 
         html.Br([]),
@@ -97,10 +104,10 @@ app.layout = html.Div(children=[
                     2000: '2000',
                     2020: '2020',
                     },
-                value=[1970, 2020]
+                value=[1980, 2020]
             ),
         ],
-        style={'width':'50%', 'display':'inline-block','margin-left':28,'margin-right': 'auto'}
+        style={'width':'60%', 'display':'inline-block','margin-left':28,'margin-right': 'auto'}
         ),
 
         html.Br([]),
@@ -125,49 +132,20 @@ app.layout = html.Div(children=[
                     4: '4',
                     5: '5'
                     },
-                value=[2, 5]
+                value=[3, 5]
             )
         ], 
-        style={'width':'50%', 'display':'inline-block','margin-left':20,'margin-right': 'auto'  }
+        style={'width':'60%', 'display':'inline-block','margin-left':20,'margin-right': 'auto'  }
         ),
 
         html.Br([]),
         html.Br([]),
 
-        html.H6('Results',
-            style={
-                'display':'inline-block',
-            }
-        ),
-
-        html.Div([
-            dcc.Slider(
-                id='output-count-slider',
-                min=20,
-                max=100,
-                step=20,
-                marks={
-                    20: '20',
-                    40: '40',
-                    60: '60',
-                    80: '80',
-                    100: '100'
-                    },
-                value=20
-            )
-        ], 
-        style={'width':'50%', 'display':'inline-block', 'margin-left':20 }
-
-        ),
+        html.Table(id='table'),
 
         html.Br([]),
-        html.Br([]),
-
-        html.Table(id='table')
-
-
     ],
-    style={'margin':25, 'text-align': 'center' ,}
+    style={'text-align': 'center' ,}
     ),
 ])
 
@@ -175,13 +153,13 @@ app.layout = html.Div(children=[
     Output(component_id='table', component_property='children'),
     [Input(component_id='movie-selector', component_property='value'),
     Input(component_id='year-slider', component_property='value'),
-    Input(component_id='rating-slider', component_property='value'),
-    Input(component_id='output-count-slider', component_property='value')]
+    Input(component_id='rating-slider', component_property='value'),]
 )
-def update_table(movie_value,year,rating,output):
+def update_table(movie_value,year,rating,):
 
     if not movie_value:
-        return html.P('Select at least one movie')
+        return [html.Tr(html.Th('Movie Recommendations')),
+                html.Tr(html.Td("""Input some of your favorite movies to discover other movies you'll love"""))]
 
     else:
 
@@ -197,14 +175,14 @@ def update_table(movie_value,year,rating,output):
 
         sim_score_df = pd.DataFrame({'matrix_row':np.arange(len(neighbors)),'sim_score':-neighbors})
 
-        table_data = df.merge(sim_score_df,on='matrix_row')
+        table_data = df.merge(sim_score_df,left_index=True,right_on='matrix_row')
 
         table_data = table_data[(~table_data.matrix_row.isin(rows))&\
         (table_data.year >= year[0])&(table_data.year <= year[1])&\
         (table_data.avg_rating >= rating[0])&(table_data.avg_rating <= rating[1])]\
         .sort_values('sim_score').reset_index()[['movie_name','avg_rating']]
 
-    return make_dash_table(table_data.iloc[:output])
+    return make_dash_table(table_data.iloc[:100],len(rows))
 
 
 if __name__ == '__main__':
